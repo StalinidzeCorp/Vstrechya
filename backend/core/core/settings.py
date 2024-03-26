101e6d6bce14dc1562ea6a8d5103f061275ad164
Karasik876 <nik.pou@mail.ru>
2024-03-26 20:38:53 +0500

    account app initialization Django REST Framework initial commit

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -15,6 +15,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account.apps.AccountConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
