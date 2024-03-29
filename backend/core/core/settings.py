3534a5db7eb45e084b80ccabb026da3bde4fde1c
Karasik876 <nik.pou@mail.ru>
2024-03-29 20:03:34 +0500

    JWT and Djoser token auth improvement

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -41,7 +41,7 @@ DJOSER = {
     'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
     'USERNAME_RESET_CONFIRM_URL': 'email/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': 'activate/{uid}/{token}',
-    'SEND_ACTIVATION_EMAIL': True,
+    'SEND_ACTIVATION_EMAIL': False,
     'SERIALIZERS': {}
 }
 
