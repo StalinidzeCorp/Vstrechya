39e3723879d30d890f23beb2471fe16d7c2d49b0
StalinidzeCorp <guidemaster228@gmail.com>
2024-06-05 19:23:31 +0500

    Update entrypoint.sh

--- backend/core/entrypoint.sh
+++ backend/core/entrypoint.sh
@@ -2,17 +2,11 @@
 echo "Collect static files"
 python manage.py collectstatic --noinput
 
-# Apply database migrations
 echo "Apply database migrations"
 until python manage.py migrate
 do
   echo "Waiting for db to be ready..."
   sleep 2
 done
-
-# --> Duplicate logs to stdout for portainer console
-echo "--> Starting prod logs"
-tail -f /logs/gunicorn/access.log > /dev/stdout &
-tail -f /logs/gunicorn/error.log > /dev/stderr &
 
 exec "$@"
