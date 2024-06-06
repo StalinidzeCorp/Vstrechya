d8dbcad3d3490f573108a444bed30aab660416f7
StalinidzeCorp <guidemaster228@gmail.com>
2024-06-06 10:12:01 +0500

    fix urls

--- backend/core/core/urls.py
+++ backend/core/core/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-]
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
