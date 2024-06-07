079538d48786b835db15f7e712ef1a8fe269cee9
StalinidzeCorp <guidemaster228@gmail.com>
2024-06-07 22:29:58 +0500

    Update settings.py

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -301,9 +301,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
