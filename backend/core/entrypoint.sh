0155aa83922926312ef2d49968c9b9ed5b8253b1
StalinidzeCorp <guidemaster228@gmail.com>
2024-06-07 23:52:43 +0500

    st

--- backend/core/entrypoint.sh
+++ backend/core/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "Collect static files"
 python manage.py collectstatic --noinput
-chmod 777 /home/app/web/static/
 
 echo "Apply database migrations"
 until python manage.py migrate
