9d384a1ce3985e5a5ae6be5bd90bf61af481f5bd
Nikita Kuzmin <guidemaster228@gmail.com>
2024-06-12 18:47:44 +0500

    Update views.py

--- backend/core/collection/views.py
+++ backend/core/collection/views.py
@@ -43,7 +43,7 @@ class CollectionsViewSet(viewsets.ViewSet):
         serializer = CollectionCreateSerializer(data=request.data)
         if serializer.is_valid():
             collection = serializer.save()
-            data = {"user": request.user, "collection": collection, "is_visible": True}
+            data = {"user": request.user, "collection": collection}
             user_collection_serializer = UserCollectionCreateSerializer(data=data)
             if user_collection_serializer.is_valid():
                 user_collection_serializer.save()
